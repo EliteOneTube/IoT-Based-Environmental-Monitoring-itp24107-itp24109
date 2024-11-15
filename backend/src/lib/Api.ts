c8cc0e66440b75d2526999756cf5d2078bf9f0f0
EliteOneTube <manos1234599@gmail.com>
2024-11-15 21:40:43 +0200

    feat: change name of endpoint

--- backend/src/lib/Api.ts
+++ backend/src/lib/Api.ts
@@ -39,7 +39,7 @@ export default class Api {
     private mountRoutes(): void {
         const router = express.Router();
 
-        router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(this.swaggerDocument));
+        router.use('/docs', swaggerUi.serve, swaggerUi.setup(this.swaggerDocument));
 
         router.get('/weather', this.authenticateToken, async (req: Request, res: Response) => {
             const weatherData = await this.database.getWeatherData();
